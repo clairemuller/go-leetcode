105c0c13050c631f3a519d7bb9a02173c7b45f26
Claire Muller <clairermuller@gmail.com>
2019-10-08 11:46:54 -0700

    solve two sum using map

--- two_sum.go
+++ two_sum.go
@@ -11,10 +11,11 @@ package main
 
 import "fmt"
 
-func twoSum(nums []int, target int) []int {
+// nested for loops
+func twoSum(nums []int, sum int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
-			if nums[i] + nums[j] == target {
+			if nums[i]+nums[j] == sum {
 				sum := []int{i, j}
 				return sum
 			}
@@ -23,9 +24,30 @@ func twoSum(nums []int, target int) []int {
 	return []int{}
 }
 
+// using a map
+func twoSumMap(nums []int, sum int) []int {
+	// initialize empty map
+	m := make(map[int]int)
+
+	// start looping thru nums
+	// target is the current number's match to fulfill sum
+	for i := 0; i < len(nums); i++ {
+		target := sum - nums[i]
+		// if the map has a key of target
+		// return the target's index and current index
+		// else create the key/value pair
+		if _, ok := m[target]; ok {
+			return []int{m[target], i}
+		} else {
+			m[nums[i]] = i
+		}
+	}
+	return []int{}
+}
+
 func main() {
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	result := twoSum(nums, target)
+	nums := []int{2, 11, 15, 7}
+	sum := 9
+	result := twoSumMap(nums, sum)
 	fmt.Println(result)
 }
