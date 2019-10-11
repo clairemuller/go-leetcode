9c66ea1c05780d84f9249349a6a76766121a021d
clairemuller <clairermuller@gmail.com>
2019-10-11 13:21:55 -0700

    set up fizz buzz

---
+++ fizz_buzz/fizz_buzz.go
@@ -0,0 +1,18 @@
+// Write a program that outputs the string representation of numbers from 1 to n.
+// But for multiples of three it should output “Fizz” instead of the number and
+// for the multiples of five output “Buzz”.
+// For numbers which are multiples of both three and five output “FizzBuzz”.
+
+package main
+
+import "fmt"
+
+func fizzBuzz(n int) []string {
+	return []string{}
+}
+
+func main() {
+	num := 9
+	result := fizzBuzz(num)
+	fmt.Println(result)
+}
